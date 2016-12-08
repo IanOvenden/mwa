ab6404024d89a0a34e9ed290880940d230abe0e8
Ian Ovenden <iovenden@wiley.com>
2016-12-08 17:11:31 +0000

    Added postCSS webpack functionality

---
+++ src/scripts/app.js
@@ -0,0 +1,3 @@
+import styles from '../css/app.css';
+// Or this if you’re not into ES6 yet:
+//var styles = require('../css/app.css');
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,40 @@
 'use strict';
 
-const webpack = require("webpack");
+const webpack = require( 'webpack' );
+const path = require( 'path' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 
 module.exports = {
-  context: __dirname + "/src",
-  entry: {
-    app: "./scripts/app.js",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: __dirname + "/dist/assets",
-    publicPath: "/assets"
-  },
-  devServer: {
-    contentBase: __dirname + "/src"
-  },
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: "./scripts/app.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist/assets'),
+        publicPath: "/assets"
+    },
+    devServer: {
+        contentBase: __dirname + "/src"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                use: [{
+                    loader: "babel-loader",
+                    options: { presets: ["es2015"] }
+                }],
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader?importLoaders=1!postcss-loader'
+                }),
+            }
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].bundle.css'),
+    ]
 };
