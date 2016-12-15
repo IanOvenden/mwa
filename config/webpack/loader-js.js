6278a25c263455d69fa2c53e5103c6963c287f76
Ian Ovenden <iovenden@wiley.com>
2016-12-15 16:36:52 +0000

    Removing babel-preset-stage-0

--- config/webpack/loader-js.js
+++ config/webpack/loader-js.js
@@ -2,6 +2,6 @@ module.exports 	= {
     test: /\.jsx$/,
     use: [{
         loader: 'babel-loader',
-        options: { presets: [ 'es2015', 'stage-0', 'react' ] }
+        options: { presets: [ 'es2015', 'react' ] }
     }]
 }
