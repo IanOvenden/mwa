f27ed18de7b461cfb481e364bedbe149ed19b2aa
Ian Ovenden <iovenden@wiley.com>
2017-01-10 18:16:42 +0000

    adding board list example and additional notes

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './css/app.css';
 
+class Board extends React.Component {
+	render() {
+		return <li>{this.props.name}</li>;
+	}
+}
+
+class BoardList extends React.Component {
+
+	render() {
+		var Console = console;
+		Console.log( this.props.boards );
+		let boards = this.props.boards.map( board => {
+			return <Board key={board.id} name={board.name}/>;
+		});
+		return (
+			<ul>
+				{boards}
+			</ul>
+		);
+	}
+}
+
 class App extends React.Component {
+
+	constructor( props ) {
+		super( props );
+		this.state = {
+			boards: [
+				{
+					id: 0,
+					name: 'snow'
+				},
+				{
+					id: 1,
+					name: 'skate'
+				}
+			]
+		};
+	}
 
 	render() {
 
@@ -12,10 +50,13 @@ class App extends React.Component {
 
 		Console.log( arr() );
 
-		let foobar = 'Reactoooorrrr!';
+		let foobar = 'Reactoooorrrr!!!';
 
 		return (
-			<p> Hello {foobar}</p>
+			<section>
+				<p> Hello {foobar}</p>
+				<BoardList boards={this.state.boards}/>
+			</section>
 		);
 	}
 }
