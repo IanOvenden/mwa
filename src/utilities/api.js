eec5b0ac70848be61a4dd69365d16c7962c24c37
Ian Ovenden <iovenden@wiley.com>
2017-02-02 15:49:24 +0000

    removing old utilities/api

--- src/utilities/api.js
+++
@@ -1,9 +0,0 @@
-const BOARDENDPOINT = 'http://private-c05f7-snapimock.apiary-mock.com/snapi/boards';
-
-export function getBoards() {
-	return fetch( BOARDENDPOINT )
-		.then( res => res.json() )
-		.catch( err => {
-			console.log( err );
-		});
-}
