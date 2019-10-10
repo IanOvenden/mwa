020522150d02fb915f85a9c43dbcb969c6044964
Ovenden <oveni@L35925MEU.local>
2019-10-10 17:17:35 +0100

    Updating functional test config and specs

--- test/functional/specs/stage.js
+++ test/functional/specs/stage.js
@@ -1,13 +1,13 @@
 describe( 'Snap page', function() {
 	it( 'should have the correct top level header', function() {
-		browser.url( 'http://localhost:8081/#/board/3' );
+		browser.url( '/#/board/3' );
 		browser.waitForExist( 'h1', 5000 );
 		var header = browser.getText( 'h1' );
 		expect( header ).to.eql( 'Board Title - 3' );
 	});
 
 	it( 'should have the correct top level header', function() {
-		browser.url( 'http://localhost:8081/#/board/3' );
+		browser.url( '/#/board/3' );
 		browser.waitForExist( '.stage-title', 5000 );
 		var stageTitle = browser.element( '.stage-title' );
 		stageTitle.setValue( 'UPDATED' );
